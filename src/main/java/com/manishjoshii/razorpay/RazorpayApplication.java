3a478ee4cf1c176254f583bfb758dea0420f1981
Manish Joshi <manishjoshi073@gmail.com>
2026-07-24 18:09:10 +0530

    feat(audit): enable JPA auditing with AuditorAware implementation

--- src/main/java/com/manishjoshii/razorpay/RazorpayApplication.java
+++ src/main/java/com/manishjoshii/razorpay/RazorpayApplication.java
@@ -2,12 +2,11 @@ package com.manishjoshii.razorpay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class })
-@EnableJpaAuditing
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 @EnableScheduling
 public class RazorpayApplication {
 
